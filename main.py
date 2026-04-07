4cdf7ee8c200c4e7e2242c11cfb84b55e5a8f62e
Nicole Decocker <edecocke@charlotte.edu>
2026-04-07 14:09:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import pandas as pd
 from dataclasses import dataclass
 from typing import List
 
-from mut_calc.mutation_rate_calculator import (
+from mutation_rate.mut_calc.mutation_rate_calculator import (
     parse_fasta,
     needleman_wunsch,
     calculate_mutation_rate,
